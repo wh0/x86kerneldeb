1bbbad2c38b242798d28d273c0632784f3bc1b58
w <none>
2026-05-02 03:43:02 +0000

    void: bump linux6.18

--- void.sh
+++ void.sh
@@ -13,14 +13,14 @@ xbps-install -Sy bash git base-devel
 git config --global --add safe.directory /root/void-packages
 cd /root/void-packages
 ln -s / masterdir
-XBPS_CHROOT_CMD=ethereal XBPS_ALLOW_CHROOT_BREAKOUT=yes ./xbps-src configure linux6.12
+XBPS_CHROOT_CMD=ethereal XBPS_ALLOW_CHROOT_BREAKOUT=yes ./xbps-src configure linux6.18
 EOF
 
 docker run --rm -i --platform linux/386 -v ./builddir:/builddir "debian:${DEBIAN_TAG:-unstable}" <<EOF
 set -eux
 apt-get update
 apt-get install -y build-essential \
-	debhelper bc bison cpio flex gcc-i686-linux-gnu kmod libelf-dev:native libssl-dev:native libssl-dev rsync
+	debhelper bc bison flex gcc-i686-linux-gnu kmod libdw-dev:native libelf-dev:native libssl-dev:native libssl-dev python3:native rsync
 cd /builddir/linux*-*
 ./scripts/config \
 	--set-str CONFIG_LOCALVERSION -1
